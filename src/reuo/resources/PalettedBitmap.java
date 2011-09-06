2d8c32a0abeb73b9992fdcd2c80eea02cc78d42a
Lucas Green <Luke@.(none)>
2011-09-05 23:18:10 -0700

    Added: initial commit of paletted bitmap

---
+++ src/reuo/resources/PalettedBitmap.java
@@ -0,0 +1,24 @@
+package reuo.resources;
+
+import java.nio.*;
+
+import reuo.util.Rect;
+
+public class PalettedBitmap extends Bitmap{
+	private Palette pal;
+	
+	public PalettedBitmap(int id, int width, int height, Palette pal, Buffer data, Rect insets) {
+		super(id, width, height, data, insets);
+		this.pal = pal;
+	}
+	
+	public PalettedBitmap(int id, int width, int height, Buffer data,
+			Rect insets) {
+		this(id, width, height, null, data, insets);
+	}
+
+	public Palette getPalette() {
+		return pal;
+	}
+	
+}
